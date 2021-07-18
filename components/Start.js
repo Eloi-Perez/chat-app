7a8f8fc86f3cf09e5e9b0f474010eecab3b7a095
Eloi-Perez <eloi104@hotmail.com>
2021-07-18 16:50:21 +0100

    UI

--- components/Start.js
+++ components/Start.js
@@ -8,38 +8,50 @@ export default function Start({ navigation: { navigate } }) {
 
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <ImageBackground source={require('../assets/background.png')} resizeMode="cover" style={styles.image} >
+            <ImageBackground source={require('../assets/background.png')} resizeMode="cover" style={styles.background} >
                 <Text style={styles.text}>Hello!</Text>
-                <TextInput
-                    style={styles.input}
-                    onChangeText={onChangeName}
-                    value={name}
-                    placeholder="your Name please"
+                <View style={styles.containerInput}>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={onChangeName}
+                        value={name}
+                        placeholder="your Name please"
                         onSubmitEditing={() => navigate('Chat', { name: name, mycolor: mycolor })}
-                />
-                <View style={styles.selectorsview}>
-                    <TouchableOpacity
-                        style={[styles.selector, { backgroundColor: '#090C08' }]}
-                        onPress={() => onChangemycolor('#090C08')}
                     />
-                    <TouchableOpacity
-                        style={[styles.selector, { backgroundColor: '#474056' }]}
-                        onPress={() => onChangemycolor('#474056')}
-                    />
-                    <TouchableOpacity
-                        style={[styles.selector, { backgroundColor: '#8A95A5' }]}
-                        onPress={() => onChangemycolor('#8A95A5')}
-                    />
-                    <TouchableOpacity
-                        style={[styles.selector, { backgroundColor: '#B9C6AE' }]}
-                        onPress={() => onChangemycolor('#B9C6AE')}
-                    />
-                </View>
-                <View style={styles.button}>
-                    <Button
-                        title="Go to Chat"
-                        onPress={() => navigate('Chat', { name: name, mycolor: mycolor })}
-                    />
+                    <Text style={styles.selectorText}>Choose your background colour</Text>
+                    <View style={styles.selectorsView}>
+                        <View style={styles.selectorsContainer}>
+                            <TouchableOpacity
+                                style={[styles.selector, { backgroundColor: '#090C08' }]}
+                                onPress={() => onChangemycolor('#090C08')}
+                            />
+                        </View>
+                        <View style={styles.selectorsContainer}>
+                            <TouchableOpacity
+                                style={[styles.selector, { backgroundColor: '#474056' }]}
+                                onPress={() => onChangemycolor('#474056')}
+                            />
+                        </View>
+                        <View style={styles.selectorsContainer}>
+                            <TouchableOpacity
+                                style={[styles.selector, { backgroundColor: '#8A95A5' }]}
+                                onPress={() => onChangemycolor('#8A95A5')}
+                            />
+                        </View>
+                        <View style={styles.selectorsContainer}>
+                            <TouchableOpacity
+                                style={[styles.selector, { backgroundColor: '#B9C6AE' }]}
+                                onPress={() => onChangemycolor('#B9C6AE')}
+                            />
+                        </View>
+                    </View>
+                    <View style={styles.button}>
+                        <Button
+                            title="Go to Chat"
+                            onPress={() => navigate('Chat', { name: name, mycolor: mycolor })}
+                            color="#8A95A5"
+                        />
+                    </View>
                 </View>
             </ImageBackground>
         </View>
@@ -47,13 +59,17 @@ export default function Start({ navigation: { navigate } }) {
 }
 
 const styles = StyleSheet.create({
-    // container: {
-    //     flex: 1,
-    // },
-    image: {
+    background: {
         flex: 1,
         justifyContent: "center",
         alignSelf: "stretch",
+        justifyContent: "space-around",
+    },
+    containerInput: {
+        backgroundColor: '#fff',
+        width: '88%',
+        alignSelf: "center",
+        padding: 15,
     },
     text: {
         color: "white",
@@ -65,24 +81,32 @@ const styles = StyleSheet.create({
     input: {
         alignSelf: "center",
         height: 40,
-        borderColor: "gray",
+        borderColor: "#8A95A5",
         borderWidth: 2,
-        width: "40%",
+        width: "100%",
         textAlign: "center",
     },
-    button: {
-        paddingTop: 10,
-        alignSelf: "center",
-        width: "40%",
+    selectorText: {
+        color: "#8A95A5",
+        textAlign: "center",
+        paddingTop: 30,
+        paddingBottom: 8,
     },
-    selectorsview: {
+    selectorsView: {
         alignSelf: "center",
         flexDirection: 'row',
-        padding: 20,
+    },
+    selectorsContainer: {
+        padding: 2,
     },
     selector: {
         width: 60,
         height: 60,
         borderRadius: 30,
+    },
+    button: {
+        paddingTop: 30,
+        alignSelf: "center",
+        width: "100%",
     },
 });
