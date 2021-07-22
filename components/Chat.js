fef5fb9864a979cdd1cf7950a52a7829b840f22a
Eloi-Perez <eloi104@hotmail.com>
2021-07-22 10:19:13 +0100

    firestore db sync

--- components/Chat.js
+++ components/Chat.js
@@ -1,40 +1,73 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, LogBox } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat'
+
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env'
+
+LogBox.ignoreLogs(['Setting a timer']);
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
+};
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+const referenceChatMessages = firebase.firestore().collection('messages');
 
 export default function Chat(props) {
     const navigation = useNavigation();
     const { name, mycolor } = props.route.params;
 
     const [messages, setMessages] = React.useState([]);
+    const [loggedInText, setLoggedInText] = React.useState('Please wait, you are getting logged in');
+    const [uid, setUid] = React.useState(0);
 
     React.useLayoutEffect(() => {
         navigation.setOptions({
-            title: name,  // title: name === '' ? 'No title' : name,
+            title: name,  // title: name === '' ? 'No title' : name, //loggedInText
         });
-    }, [navigation, name]);
+    }, [navigation, name, loggedInText]);
 
     React.useEffect(() => {
-        setMessages([
-            {
-                _id: 1,
-                text: 'Hello developer',
-                createdAt: new Date(),
-                user: {
-                    _id: 2,
-                    name: 'React Native',
-                    avatar: 'https://placeimg.com/140/140/any',
-                },
-            },
-            {
-                _id: 2,
-                text: 'This is a system message',
-                createdAt: new Date(),
-                system: true,
-            },
-        ])
-    }, []);
+        async function firebaseconnection() {
+            try {
+                await firebase.auth().onAuthStateChanged(async (user) => {
+                    if (!user) {
+                        await firebase.auth().signInAnonymously();
+                    }
+                    setUid(user.uid);
+                    setLoggedInText('Hello ' + user.uid);
+                    await referenceChatMessages.orderBy("createdAt", "desc").onSnapshot(querySnapshot => { //.where("uid", "==", user.uid)
+                        const toMessages = [];
+                        querySnapshot.forEach((doc) => {
+                            let data = doc.data();
+                            toMessages.push({
+                                _id: data._id,
+                                text: data.text,
+                                createdAt: data.createdAt.toDate(),
+                                user: data.user,
+                            });
+                        });
+                        setMessages(toMessages);
+                    });
+                });
+            } catch (e) {
+                console.error(e);
+            }
+        }; firebaseconnection();
+        //stop listening on unmount
+        return () => { firebaseconnection(); }
+    }, []); //once
 
     const renderBubble = (props) => (
         <Bubble
@@ -46,8 +79,8 @@ export default function Chat(props) {
             //     right: {},
             // }}
             wrapperStyle={{
-                left: {},
-                right: { backgroundColor: '#000' },
+                left: { backgroundColor: '#e6dc6e'},
+                right: { backgroundColor: '#6ed6e6' },
             }}
         // bottomContainerStyle={{
         //     left: { borderColor: 'purple', borderWidth: 4 },
@@ -67,6 +100,7 @@ export default function Chat(props) {
     );
 
     const onSend = (newMessages = []) => {
+        newMessages.map(msg => referenceChatMessages.add(msg));
         setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessages));
     };
 
@@ -76,7 +110,7 @@ export default function Chat(props) {
             messages={messages}
             onSend={onSend}
             user={{
-                _id: 1,
+                _id: uid,
                 name: name,
             }}
             renderBubble={renderBubble}
@@ -92,5 +126,4 @@ const styles = StyleSheet.create({
     //     justifyContent: 'center',
     //     alignItems: 'center',
     // },
-
 });
