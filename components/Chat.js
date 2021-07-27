8fc6a2735fba0257bbe32a2a5f8a9f63a11f581f
Eloi-Perez <eloi104@hotmail.com>
2021-07-27 02:26:36 +0100

    custom actions

--- components/Chat.js
+++ components/Chat.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import { LogBox } from 'react-native';
+import { LogBox, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat'
+import Lightbox from 'react-native-lightbox-v2';
+import 'react-native-get-random-values'
+import { v4 as uuidv4 } from 'uuid';
 
-import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env'
-
-LogBox.ignoreLogs(['Setting a timer']);
-// LogBox.ignoreAllLogs();
+import { API_KEY, PROJECT_ID, MESSAGING_SENDER_ID, APP_ID } from '@env'
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+// import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
+import 'firebase/compat/firestore';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import NetInfo from '@react-native-community/netinfo';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+LogBox.ignoreLogs(['Setting a timer']);
+// LogBox.ignoreAllLogs();
+
+import MapView, { Marker } from 'react-native-maps';
+import CustomActions from './CustomActions';
+
 
 // initialize Firebase Firestore
 const firebaseConfig = {
     apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
+    authDomain: `${PROJECT_ID}.firebaseapp.com`,
     projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
+    storageBucket: `${PROJECT_ID}.appspot.com`,
     messagingSenderId: MESSAGING_SENDER_ID,
     appId: APP_ID,
 };
@@ -95,27 +105,38 @@ export default function Chat(props) {
     React.useEffect(() => { saveMessages() }, [messages]);
 
     // firestore listening function
-    const firestoreConnection = () => firebase.auth().onAuthStateChanged(async (user) => {
-        let previousText = loggedInText;
-        setLoggedInText('Loading...');
-        if (!user) { await firebase.auth().signInAnonymously(); }
-        setUid(user.uid);
-        await AsyncStorage.setItem('uid', user.uid);
-        referenceChatMessages.orderBy("createdAt", "desc").onSnapshot(querySnapshot => {
-            const toMessages = [];
-            querySnapshot.forEach((doc) => {
-                let data = doc.data();
-                toMessages.push({
-                    _id: data._id,
-                    text: data.text,
-                    createdAt: data.createdAt.toDate(),
-                    user: data.user,
+    const firestoreConnection = async () => {
+        const auth = getAuth();
+        signInAnonymously(auth)
+        onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                let previousText = loggedInText;
+                setLoggedInText('Loading...');
+                setUid(user.uid);
+                await AsyncStorage.setItem('uid', user.uid);
+                referenceChatMessages.orderBy("createdAt", "desc").onSnapshot(querySnapshot => {
+                    const toMessages = [];
+                    querySnapshot.forEach((doc) => {
+                        let data = doc.data();
+                        toMessages.push({
+                            _id: data._id,
+                            text: data.text || null,
+                            createdAt: data.createdAt.toDate(),
+                            user: {
+                                _id: data.user._id,
+                                name: data.user.name,
+                                avatar: data.user.avatar || null,
+                            },
+                            image: data.image || null,
+                            location: data.location || null,
+                        });
+                    });
+                    setMessages(toMessages);
                 });
-            });
-            setMessages(toMessages);
+                setLoggedInText(previousText);
+            }
         });
-        setLoggedInText(previousText);
-    });
+    }
 
     // loading messages and online status
     React.useEffect(() => {
@@ -127,7 +148,7 @@ export default function Chat(props) {
             if (netState.isConnected) { firestoreConnection() }
         });
         // stop listening on unmount
-        return () => { netStateSubscription(); }
+        return () => { netStateSubscription(); console.log('unmounted NetInfo'); }
     }, []); // once
 
     // input send to firestore + State
@@ -189,6 +210,68 @@ export default function Chat(props) {
         />
     );
 
+
+    // Pattern to send media messages
+    const onSendFromUser = (messages = []) => {
+        const createdAt = new Date();
+        const messagesToUpload = messages.map(msg => {
+            return msg = {
+                _id: uuidv4(),
+                text: msg.text || null,
+                createdAt: createdAt,
+                user: {
+                    _id: uid,
+                    name: name,
+                    avatar: `https://ui-avatars.com/api/?background=random&name=${name}`,
+                },
+                image: msg.image || null,
+                location: msg.location || null,
+            }
+        })
+        onSend(messagesToUpload)
+        // const messagesToUpload = messages.map(message => ({
+        //     ...message,
+        //     user,
+        //     createdAt,
+        //     _id: Math.round(Math.random() * 1000000),
+        // }))
+    }
+    const renderCustomActions = props =>
+        Platform.OS === 'web' ? null : (
+            <CustomActions {...props} onSend={onSendFromUser} />
+        )
+
+    const renderCustomView = props => {
+        const { currentMessage } = props;
+        if (currentMessage.location) {
+            return (
+                <MapView
+                    style={{
+                        width: 150,
+                        height: 100,
+                        borderRadius: 13,
+                        margin: 3
+                    }}
+                    region={{
+                        latitude: currentMessage.location.latitude,
+                        longitude: currentMessage.location.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                >
+                    <Marker
+                        coordinate={{
+                            latitude: currentMessage.location.latitude,
+                            longitude: currentMessage.location.longitude
+                        }}
+                    />
+                </MapView>
+            );
+        }
+        return null;
+    }
+
+
     return (
         <GiftedChat
             messages={messages}
@@ -196,10 +279,15 @@ export default function Chat(props) {
             user={{
                 _id: uid,
                 name: name,
+                avatar: `https://ui-avatars.com/api/?background=random&name=${name}` // make selectable in start
             }}
             renderBubble={renderBubble}
             renderInputToolbar={renderInputToolbar}
             messagesContainerStyle={{ backgroundColor: mycolor }}
+            renderActions={renderCustomActions}
+            renderCustomView={renderCustomView}
+            messageIdGenerator={uuidv4}
+        // renderUsernameOnMessage={true}
         />
     )
 
---
+++ components/mediaUtils.js
@@ -0,0 +1,137 @@
+// import * as Linking from 'expo-linking';
+import { Linking } from 'expo'
+import * as Location from 'expo-location'
+import * as ImagePicker from 'expo-image-picker'
+import { Alert } from 'react-native'
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+
+
+export default async function getPermissionAsync(permKey) {
+    const permission = () => {
+        switch (permKey) {
+            case 'location':
+                return Location.requestForegroundPermissionsAsync();
+            case 'storage':
+                return ImagePicker.requestMediaLibraryPermissionsAsync();
+            case 'camera':
+                return ImagePicker.getCameraPermissionsAsync();
+        }
+    }
+    const { status } = await permission();
+    if (status !== 'granted') {
+        // const permissionName = permission.toLowerCase().replace('_', ' ')
+        Alert.alert(
+            'Cannot be done 😞',
+            `If you would like to use this feature, you'll need to enable the ${permKey} permission in your phone settings.`,
+            [
+                {
+                    text: "Let's go!",
+                    onPress: () => Linking.openSettings(),
+                },
+                { text: 'Nevermind', onPress: () => { }, style: 'cancel' },
+            ],
+            { cancelable: true },
+        )
+        return false
+    }
+    return true
+}
+
+export async function getLocationAsync(onSend) {
+    if (await getPermissionAsync('location')) {
+        const location = await Location.getCurrentPositionAsync({})
+        if (location) {
+            onSend([{ location: location.coords }])
+        }
+    }
+}
+
+export async function pickImageAsync(onSend) {
+    if (await getPermissionAsync('storage')) {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            // allowsEditing: true,
+            // aspect: [4, 3],
+        })
+
+        if (!result.cancelled) {
+            const imageUrl = await this.uploadImageFetch(result.uri);
+            onSend([{ image: imageUrl }])
+            return imageUrl
+        }
+    }
+}
+
+export async function takePictureAsync(onSend) {
+    if (await getPermissionAsync('camera')) {
+        const result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            // allowsEditing: true,
+            // aspect: [4, 3],
+        })
+
+        if (!result.cancelled) {
+            const imageUrl = await this.uploadImageFetch(result.uri);
+            onSend([{ image: imageUrl }])
+            return imageUrl
+        }
+    }
+}
+
+// uploads images to Firebase in blob format
+// uploadImageFetch = async (uri) => {
+//     const blob = await new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.onload = function () {
+//             resolve(xhr.response);
+//         };
+//         xhr.onerror = function (e) {
+//             console.log(e);
+//             reject(new TypeError('Network request failed'));
+//         };
+//         xhr.responseType = 'blob';
+//         xhr.open('GET', uri, true);
+//         xhr.send(null);
+//     });
+//     const imageNameBefore = uri.split('/');
+//     const imageName = imageNameBefore[imageNameBefore.length - 1];
+//     const ref = firebase.storage().ref().child(`images/${imageName}`); // `images/${imageName}`
+//     const snapshot = await ref.put(blob);
+//     blob.close();
+//     return await snapshot.ref.getDownloadURL();
+// };
+
+uploadImageFetch = async (uri) => {
+    const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            resolve(xhr.response);
+        };
+        xhr.onerror = function (e) {
+            console.log(e);
+            reject(new TypeError('Network request failed'));
+        };
+        xhr.responseType = 'blob';
+        xhr.open('GET', uri, true);
+        xhr.send(null);
+    });
+    const imageNameBefore = uri.split('/');
+    const imageName = imageNameBefore[imageNameBefore.length - 1];
+    const storage = getStorage();
+    const storageRef = ref(storage, `images/${imageName}`);
+    let imageUrl;
+    await uploadBytes(storageRef, blob).then(async (snapshot) => {
+        await getDownloadURL(storageRef).then((url) => {
+            imageUrl = url;
+        }).catch((err) => {
+            console.error(err)
+        });
+    }).catch((err) => {
+        console.error(err)
+    });
+    blob.close();
+    return imageUrl;
+};
